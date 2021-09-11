8defd185cfccfcd16ea6eb5d292e76c29e0aff3d
LimJeonghyun <22000617@handong.edu>
2021-09-12 01:45:06 +0900

    lab2

---
+++ src/lab2/fruit/Main.java
@@ -0,0 +1,39 @@
+package lab2.fruit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	public static void main(String args[]) {
+		List<Fruit> fl = new ArrayList<Fruit>();
+		System.out.println("Frui List (ordered by name)");
+
+		Fruit s1 = new Fruit(101, "Apple", 1000);
+		fl.add(s1);
+		Fruit s2 = new Fruit(102, "Orange", 1500);
+		fl.add(s2);
+		fl.add(new Fruit(103, "Kiwi", 2000));
+		fl.add(new Fruit(104, "Banana", 700));
+		fl.add(new Fruit(105, "Melon", 10000));
+		fl.add(new Fruit(106, "Watermelon", 20000));
+		fl.add(new Fruit(107, "Strawberry", 3000));
+		fl.add(new Fruit(108, "Cherry", 5000));
+		fl.add(new Fruit(109, "Peach", 9000));
+		fl.add(new Fruit(110, "Blueberry", 100));
+
+		Collections.sort(fl, new FruitComparator());
+
+		for (Fruit s : fl) {
+			System.out.println(s.toString());
+		}
+
+		System.out.println("Fruit List (reverse ordered by name)");
+
+		Collections.sort(fl, new FruitComparatorDesc());
+		for (int i = 0; i < fl.size(); i++) {
+			System.out.println(fl.get(i).toString());
+		}
+
+	}
+}
